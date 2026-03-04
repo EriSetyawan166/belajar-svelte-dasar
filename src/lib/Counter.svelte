d11058f078f41ac679555ec736799aff8c6c41a9
erisetyawan166 <erisetyawan34@gmail.com>
2026-03-04 20:37:43 +0700

    derived

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
   let count = $state(0)
   const increment = () => {
     count += 1
@@ -7,4 +7,18 @@
 
 <button onclick={increment}>
   count is {count}
-</button>
+</button> -->
+
+<script>
+  let count = $state([]);
+
+  let total = $derived(count.reduce((a, b) => a + b, 0));
+
+  function increment() {
+    count.push(1);
+  }
+</script>
+
+<h1>{count.join("+")} = {total}</h1>
+
+<button onclick={increment}> increment! </button>
